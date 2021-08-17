e27ae1d3422424369112fc81ca376a8720b036d1
Alexandr tataritsky <luckonly@gmail.com>
2021-08-17 16:29:02 +0300

    Added test auth by secured, rolesAllowed, postAuthorize

--- src/main/java/ru/netology/hibernate/config/SecurityConfig.java
+++ src/main/java/ru/netology/hibernate/config/SecurityConfig.java
@@ -1,11 +1,34 @@
 package ru.netology.hibernate.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication().withUser("user_read")
+                .password(passwordEncoder.encode("123"))
+                .roles("READ");
+        auth.inMemoryAuthentication().withUser("user_write")
+                .password(passwordEncoder.encode("123"))
+                .roles("WRITE");
+
+        auth.inMemoryAuthentication().withUser("user_delete")
+                .password(passwordEncoder.encode("123"))
+                .roles("DELETE");
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,4 +37,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .authorizeRequests().anyRequest().authenticated();
     }
+
+
 }
--- src/main/java/ru/netology/hibernate/controller/Controller.java
+++ src/main/java/ru/netology/hibernate/controller/Controller.java
@@ -1,11 +1,16 @@
 package ru.netology.hibernate.controller;
 
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.netology.hibernate.service.PersonService;
 import ru.netology.hibernate.entity.Person;
 
+import javax.annotation.security.RolesAllowed;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,19 +29,29 @@ public class Controller {
     }
 
     @GetMapping("/persons/by-city")
+    @Secured("ROLE_READ")
     public List<Person> getPersonsByCity(@RequestParam(name = "city") String city) {
         return personService.getByCity(city);
     }
 
     @GetMapping("/persons/by-age")
+    @RolesAllowed("ROLE_WRITE")
     public List<Person> getPersonsByAge(@RequestParam(name = "age") int age) {
         return personService.getByAgeIsLessThan(age);
     }
 
     @GetMapping("/persons/by-name-and-surname")
-    public Optional<Person> getPersonsByNameAndSurname(@RequestParam(name = "name") String name,
+    @PreAuthorize("hasAnyRole('ROLE_DELETE, ROLE_WRITE')")
+    public Optional<Person> getPersonsByNameAndSurname(@RequestParam(name = "username") String username,
+                                        @RequestParam(name = "name") String name,
                                      @RequestParam(name = "surname") String surname) {
         return personService.getPersonsByNameAndSurname(name, surname);
     }
+
+    @GetMapping("/hello2")
+    @PostAuthorize("#username == authentication.principal.username")
+    public String getHelloMessage2(@RequestParam(name = "username") String username) {
+            return "Hello!";
+        }
 
 }
